5dcd686aca4f5badc85ded499e807873dbfdb215
Solo-Yulchik <surovuiyulchik23@gmail.com>
2025-05-11 00:20:59 +0200

    js1

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function getElementWidth(content, padding, border) {
+  const numContent = Number.parseInt(content);
+  const numPadding = Number.parseInt(padding);
+  const numBorder = Number.parseFloat(border);
+  return numContent + numPadding * 2 + numBorder * 2;
+}
+
+console.log(getElementWidth('50px', '8px', '4px'));
+console.log(getElementWidth('60px', '12px', '8.5px'));
+console.log(getElementWidth('200px', '0px', '0px'));
